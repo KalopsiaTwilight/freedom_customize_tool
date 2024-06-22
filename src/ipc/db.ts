688321c10342d28be276d087c7aca21f09d7085f
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2024-06-22 19:27:13 +0200

    Add debug logging to db calls

--- src/ipc/db.ts
+++ src/ipc/db.ts
@@ -1,5 +1,6 @@
 import { ipcMain } from "electron";
 import { Database } from "sqlite3";
+import log from "electron-log"
 
 import { getAsync, allAsync, execAsync } from "../utils";
 import { CallDbAllChannel, CallDbExecChannel, CallDbGetChannel } from "./channels";
@@ -7,7 +8,9 @@ import { CallDbAllChannel, CallDbExecChannel, CallDbGetChannel } from "./channel
 export const setupDbIpc = (db: Database) => {
   ipcMain.handle(CallDbAllChannel, async (_, query: string, ...params: any[]) => {
     try {
+      log.debug("Executing getAll query: ", query);
       const resp = await allAsync(db, query, ...params);
+      log.debug("finished getAll query.");
       return { result: resp, error: undefined }
     } catch (error) {
       return { result: undefined, error }
@@ -15,7 +18,9 @@ export const setupDbIpc = (db: Database) => {
   });
   ipcMain.handle(CallDbGetChannel, async (_, query: string, ...params: any[]) => {
     try {
+      log.debug("Executing get query: ", query);
       const resp = await getAsync(db, query, ...params);
+      log.debug("Finished get query.");
       return { result: resp, error: undefined }
     } catch (error) {
       return { result: undefined, error }
@@ -23,7 +28,9 @@ export const setupDbIpc = (db: Database) => {
   });
   ipcMain.handle(CallDbExecChannel, async (_, query: string) => {
     try {
+      log.debug("Executing exec query: ", query);
       const resp = await execAsync(db, query);
+      log.debug("Finished exec query: ", query);
       return { result: resp, error: undefined }
     } catch (error) {
       return { result: undefined, error }
